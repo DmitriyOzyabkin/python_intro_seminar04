bcc5febdf37f426f8c2573677e77836c05ba6307
Dmitriy Ozyabkin <ozyabkin.d@gmail.com>
2024-08-11 20:49:18 +0300

    Task04 completed

--- HW04.py
+++ HW04.py
@@ -1,19 +1,30 @@
-def create_oroginal_dict(text: str) -> dict:
-    pass
+from pprint import pprint
+
+def create_original_dict(text: str) -> dict:
+    original_dict={}
+    for sym in text:
+        original_dict[sym] = original_dict.get(sym, 0) + 1
+    return original_dict
 
 def invert_dict(original_dict: dict) -> dict:
-    pass
+    invert_dict = {}
+    for letter in original_dict.keys():
+        frec = original_dict[letter]
+        if frec not in invert_dict:
+            invert_dict[frec] = [letter]
+        else:
+            invert_dict[frec].append(letter)
+    return invert_dict
 
 
-data = '''Создайте программу для лингвистов, которая будет инвертировать полученный
-словарь. То есть в качестве ключа будет частота, а в качестве значения —
-список символов с этой частотой.'''
 
+data = input('Введте текст: ') 
 
 data = data.lower()
-letter_frec_dict = create_oroginal_dict(data)
-
+letter_frec_dict = create_original_dict(data)
 inv_dict = invert_dict(letter_frec_dict)
 
-print(letter_frec_dict)
-print(inv_dict)
+print('Оригинильный словарь частот:')
+pprint(letter_frec_dict)
+print('Инвертированный словарь частот:')
+pprint(inv_dict)
